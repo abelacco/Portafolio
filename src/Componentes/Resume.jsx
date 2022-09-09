0868242647e7a5f49f3a9ba2ee3e5415a8352745
abelacco <abel3121@gmail.com>
2022-09-09 08:28:54 -0500

    con resumen en proceso

--- src/Componentes/Resume.jsx
+++ src/Componentes/Resume.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import style from "../styles/Resume.module.css"
+import js from "../Icons/js.png"
+import node from "../Icons/nodejs.png"
+import mongo from "../Icons/mongodb_plain_wordmark_logo_icon_146423.png"
+import express from "../Icons/express.png"
+import react from "../Icons/react.png"
 
 export default function EducationSkills() {
   return (
-    <div>EducationSkills</div>
+    <div className={style.Skills}>
+      <h1>Skills</h1>
+      <div className={style.allSkills}>
+        <div className={style.Tech}>
+          <h3>Technical Skills</h3>
+          <div className={style.imagenes}>
+            <img src={mongo} alt="cargando"/>
+            <img src={express} alt="cargando"/>
+            <img src={react} alt="cargando"/>
+            <img src={node} alt="cargando"/>
+            <img src={js} alt="cargando"/>
+            
+           
+          </div>
+       
+        </div>
+        
+      </div>
+    </div>
   )
 }
