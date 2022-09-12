3a9990f4e2145532add924c90546459ac7f2e0ae
abelacco <abel3121@gmail.com>
2022-09-12 07:22:23 -0500

    en home se agrego botones de git y LK

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import style from "../styles/Home.module.css"
+import linkedin from "../Icons/linkedin.png"
+import gitHub from "../Icons/gitHub.png"
 
 
 export default function Home() {
@@ -12,6 +14,10 @@ export default function Home() {
      <span className={style.Title}>Full Stack Developer</span>
      </h1> 
      <p>I build scalable end-to-end web solutions. I am passionate about learning new technologies and solving problems to help teams.</p>
+     <div className={style.divImg}>
+     <a className={style.A} href="https://www.linkedin.com/in/abelacunacoronado/" target="__blank"><img className={style.img} src={linkedin} alt="linkedind"/></a>
+     <a className={style.A} href="https://github.com/abelacco" target="__blank"><img className={style.img} src={gitHub} alt="linkedind"/></a>
+     </div>
     </div>
     
   )
